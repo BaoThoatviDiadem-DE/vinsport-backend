e5e7b53065a2e7d9aa51ddaa8e2adbf8ce756e50
chuducnhatminhcoding <chuducnhatminh@gmail.com>
2026-04-18 04:05:30 +0000

    sua vite-env

--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_USE_MOCK: string;
+  readonly VITE_API_URL: string; // Thêm các biến khác của em vào đây
+  // Thêm bất kỳ biến nào bắt đầu bằng VITE_ mà em có trong file .env
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
